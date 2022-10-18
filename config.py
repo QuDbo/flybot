e9d51c84d638389c7e7f70c409b1aaa35a4e5eaa
QuDbo <quentin.desbonnets@gmail.com>
2022-10-18 10:40:18 +0200

    change port

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ import os
 class DefaultConfig:
     """Configuration for the bot."""
 
-    PORT = 3978
+    PORT = 58475
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")   
     LUIS_APP_ID = os.environ.get("LuisAppId", "")
